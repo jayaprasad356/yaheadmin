e8bd0a80cb97a69c8ea0c1270db868bc253b77c0
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-19 19:17:07 +0530

    crud

--- includes/crud.php
+++ includes/crud.php
@@ -19,9 +19,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "yahe";    // Change as required
+    private $db_user = "u743445510_yahe";  // Change as required
+    private $db_pass = "Yahe@2022";  // Change as required
+    private $db_name = "u743445510_yahe";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
